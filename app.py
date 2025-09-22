57820d4b2bb2e83135867f9969c80b00d09efc68
ankitchawla071 <81257637+ankitchawla071@users.noreply.github.com>
2025-09-22 23:53:24 +0530

    app.py

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def data():
         return jsonify({"temp": last["temp"]})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port)
